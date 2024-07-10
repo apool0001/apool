27636f70869e246af9518338462a42af09477bbb
apool0001 <apool0001@outlook.com>
2024-07-11 00:52:28 +0530

    Update and rename apool.sh to mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,3 @@
 wget https://github.com/apool-io/apoolminer/releases/download/v1.6.10/apoolminer_linux_v1.6.10.tar
 tar xvf apoolminer_linux_v1.6.10.tar
-./apoolminer --account CP_7cqdjrvl2n --gpu-off --pool qubic1.hk.apool.io3334
+./apoolminer --account CP_7cqdjrvl2n --gpu-off --pool qubic1.hk.apool.io:3334
